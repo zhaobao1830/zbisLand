49d6412b1bdb313fed030d4c54c90d182019ee04
zhaobao1830 <x5219438>
2019-06-12 19:48:27 +0800

    node里的中间件，上下文，next

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const Koa = require('koa')
+
+const app = new Koa()
+
+// 应用程序对象 有很多中间件
+
+// 发送HTTP KOA 接收HTTP(使用中间件，中间件实际就是函数)
+
+// 注册
+app.use(async (ctx, next) => {
+  // ctx 上下文
+  console.log('1')
+  const a = next()
+  console.log(a)
+  console.log('2')
+})
+
+app.use(async (ctx, next) => {
+  console.log('3')
+  console.log('4')
+  return 'abc'
+})
+
+app.listen(3300)
+

a2f41ecfcbcd704a5a540b7f4e11f2929c57cef1
zhaobao1830 <373098777@qq.com>
2019-06-18 22:59:28 +0800

    使用require-directory，按模块引入router

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const Koa = require('koa')
-const book = require('./api/v1/book')
-const classic = require('./api/v1/classic')
+const requireDirectory = require('require-directory')
+const Router = require('koa-router')
+const book = require('./app/api/v1/book')
+const classic = require('./app/api/v1/classic')
 
 const app = new Koa()
 
-app.use(book.routes())
-app.use(classic.routes())
+const modules = requireDirectory(module, './app/api/v1', {
+  visit: whenLoadModule
+})
+
+function whenLoadModule(obj) {
+  if(obj instanceof Router) {
+    app.use(obj.routes())
+  }
+}
+
+// app.use(book.routes())
+// app.use(classic.routes())
 
 app.listen(3300)
 

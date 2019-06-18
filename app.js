04181b843d696c1e0bf90457f1839d65f96f7247
zhaobao1830 <373098777@qq.com>
2019-06-18 23:22:18 +0800

    优化代码

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const Koa = require('koa')
 const requireDirectory = require('require-directory')
 const Router = require('koa-router')
-const book = require('./app/api/v1/book')
-const classic = require('./app/api/v1/classic')
 
 const app = new Koa()
 
+// 通过requireDirectory获取app/api/v1下的所有routers
+// visit: whenLoadModule 函数
 const modules = requireDirectory(module, './app/api/v1', {
   visit: whenLoadModule
 })
 
+// 用来判断引入的是router
 function whenLoadModule(obj) {
   if(obj instanceof Router) {
     app.use(obj.routes())
   }
 }
-
-// app.use(book.routes())
-// app.use(classic.routes())
 
 app.listen(3300)
 

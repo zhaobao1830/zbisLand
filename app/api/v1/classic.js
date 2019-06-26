65fe54c3f69452c05646c4b7a787d4d51e2945f8
zhaobao1830 <x5219438>
2019-06-26 15:44:17 +0800

    优化代码

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,19 +1,13 @@
 const Router = require('koa-router')
 const router = new Router()
-const { HttpException, ParameterException } = require('../../../core/http-exception')
 
 router.post('/v1/:id/classic/latest', (ctx, next) => {
-
-  const path = ctx.params  // 获取的是:id里的值
-  const query = ctx.request.query // 获取的是?param的值
-  const headers = ctx.request.header // header传递的值
-  const body = ctx.request.body // body里的值，json格式
-
   if (true) {
     // const error = new ParameterException()
     const error = new global.errs.ParameterException()
     throw error
   }
+
   ctx.body = {
     key: 'classic'
   }
--- middlewares/exception.js
+++ middlewares/exception.js
@@ -2,9 +2,11 @@ const { HttpException } = require('../core/http-exception')
 
 const catchError = async (ctx, next) => {
   try {
-    await next()
+    await next() // 有了next，函数调用后就会触发
   } catch (error) {
+    // 判断error是否是HttpException
     if (error instanceof HttpException) {
+      // 返回的错误信息
       ctx.body = {
         mag: error.msg,
         error_code: error.errorCode,

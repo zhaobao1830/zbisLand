8fe85c4970cc735083913363a6d7a140aee06751
zhaobao1830 <x5219438>
2019-08-06 19:49:44 +0800

    新建favor  model

---
+++ app/models/favor.js
@@ -0,0 +1,31 @@
+const {
+  sequelize
+} = require('../../core/db')
+
+const {
+  Sequelize,
+  Model
+} = require('sequelize')
+
+class Favor extends Model{
+  static async like(art_id, type, uid) {
+
+  }
+
+  static async dislike(art_id, type, uid) {
+
+  }
+}
+
+Favor.init({
+  uid: Sequelize.INTEGER,
+  art_id: Sequelize.INTEGER,
+  type: Sequelize.INTEGER
+}, {
+  sequelize,
+  tableName: 'favor'
+})
+
+module.exports = {
+  Favor
+}

6aa2c80e5e386e9901e73a4f30236a9bad6ea771
zhaobao1830 <x5219438>
2019-08-06 08:58:59 +0800

    flow(每期)

--- app/models/flow.js
+++ app/models/flow.js
@@ -12,7 +12,11 @@ class Flow extends Model {
 Flow.init({
   index: Sequelize.INTEGER, // 序号:比如第一期、第二期
   art_id: Sequelize.INTEGER, // 是外键，用来关联相应的实体表
-})
+},
+  {
+    sequelize,
+    tableName: 'flow'
+  })
 
 module.exports = {
   Flow

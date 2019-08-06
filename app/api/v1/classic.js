6b26644a7795c0fb40289b91a300d09e6e951496
zhaobao1830 <x5219438>
2019-08-06 08:57:13 +0800

    flow(每期)

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -2,21 +2,31 @@ const Router = require('koa-router')
 const router = new Router({
   prefix: '/v1/classic'
 })
-const { PositiveIntegerValidator } = require('../../validators/validator')
 const { Auth } = require('../../../middlewares/auth')
+const {
+  Flow
+} = require("../../models/flow")
 
 // auth也是一个中间件，一定要写在后面的中间件前面，这样才能阻止后面的中间件
 // 可以在new Auth()里传递值，确定访问当前接口需要什么权限  new Auth(2)
 router.get('/latest', new Auth().m, async (ctx, next) => {
 
-  const v = new PositiveIntegerValidator().validate(ctx)
-  // parsed 是否取解析后的参数，默认为 true
-  // http://doc.cms.7yue.pro/lin/server/koa/validator.html#%E4%BD%BF%E7%94%A8
-  const id = await v.get('path.id', parsed = false) // path、query、body、header
-  ctx.body = {
-    msg: 'success',
-    id: v.get('path.id')
-  }
+  // const v = new PositiveIntegerValidator().validate(ctx)
+  // // parsed 是否取解析后的参数，默认为 true
+  // // http://doc.cms.7yue.pro/lin/server/koa/validator.html#%E4%BD%BF%E7%94%A8
+  // const id = await v.get('path.id', parsed = false) // path、query、body、header
+  // ctx.body = {
+  //   msg: 'success',
+  //   id: v.get('path.id')
+  // }
+
+  const flow = await Flow.findOne({
+    order: [
+      ['index', 'DESC']
+    ]
+  })
+
+  ctx.body = flow
 })
 
 module.exports = router
